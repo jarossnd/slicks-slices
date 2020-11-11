1f1cf78731e6f1a3aa3850834ee375156555d6e0
Jason Ross <jaross@microsoft.com>
2020-11-11 10:03:40 -0600

    M2L6

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -3,9 +3,8 @@ import Nav from '../components/Nav';
 
 export default function BeersPage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Hey! I'm the beers page</p>
-    </div>
+    </>
   );
 }
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Layout from '../components/Layout';
 import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Hey! I'm the home page eeeeeeeeeeeeeee</p>
-    </div>
+      <p>hey i'm another element</p>
+    </>
   );
 }

ab6c3eeb900b1683af97604407200303e4354d33
jason@jasonross.xyz <jason@jasonross.xyz>
2020-11-13 07:11:31 -0600

    M3L10

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/Users/jason/Dropbox/Dev/slicks-slices/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
